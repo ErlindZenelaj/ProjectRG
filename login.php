48ca7a94f9b76692d8cd8f44ea389c3b9d3019f2
Erlind <81335694+ErlindZenelaj@users.noreply.github.com>
2022-01-24 23:52:53 +0100

    LoginForm

---
+++ login.php
@@ -0,0 +1,120 @@
+<?php
+session_start();
+if(isset($_SEESION['username'])){
+	header("location:Index.php");
+}else{
+}
+?>
+		
+
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="style1.css">
+    <!-- Link per Icona -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+   </head>
+<body>
+  <div class="container">
+    <input type="checkbox" id="flip">
+    <div class="cover">
+      <div class="front">
+        <img src="img/rg.png" alt="">
+        <div class="text">
+          <span class="text-1">Every new movie is a <br> new adventure</span>
+          <span class="text-2">Let's get connected</span>
+        </div>
+      </div>
+      <div class="back">
+        <img class="backImg" src="img/rg.png" alt="">
+        <div class="text">
+          <span class="text-1">Every new movie is a<br> new adventure</span>
+          <span class="text-2">Let's get started</span>
+        </div>
+      </div>
+    </div>
+    <div class="forms">
+        <div class="form-content">
+          <div class="login-form">
+            <div class="title">Login</div>
+          <form action="login.php" method="POST">
+            <div class="input-boxes">
+              <div class="input-box">
+                <i class="fas fa-envelope"></i>
+                <input type="text" name="username" placeholder="Enter your name or email" required>
+              </div>
+              <div class="input-box">
+                <i class="fas fa-lock"></i>
+                <input type="password" name="password" placeholder="Enter your password" required>
+              </div>
+              <div class="text"><a href="#">Forgot password?</a></div>
+              <div class="button input-box">
+                <input type="submit" value="Log In" name="loginButton">
+              </div>
+              <div class="text sign-up-text">Don't have an account? <label for="flip">Sigup now</label></div>
+            </div>
+        </form>
+      </div>
+        <div class="signup-form">
+          <div class="title">Signup</div>
+        <form action="#">
+            <div class="input-boxes">
+              <div class="input-box">
+                <i class="fas fa-user"></i>
+                <input type="text" placeholder="Enter your name" required>
+              </div>
+              <div class="input-box">
+                <i class="fas fa-envelope"></i>
+                <input type="text" placeholder="Enter your email" required>
+              </div>
+              <div class="input-box">
+                <i class="fas fa-lock"></i>
+                <input type="password" placeholder="Enter your password" required>
+              </div>
+              <div class="button input-box">
+                <input type="submit" value="Sumbit">
+              </div>
+              <div class="text sign-up-text">Already have an account? <label for="flip">Login now</label></div>
+            </div>
+      </form>
+    </div>
+    </div>
+    </div>
+  </div>
+</body>
+</html>
+
+     
+
+
+
+
+
+<?php
+$username='';
+$password='';
+
+if(isset($_POST['loginButton'])){
+if(empty($_POST['username']) || empty($_POST['password'])){
+	echo "All fields are required!";
+}else{
+	$username=$_POST['username'];
+	$password=$_POST['password'];
+
+	if($username == 'Erlind' && $password == 'Zenelaj'){
+		session_start();
+		$_SESSION['username']=$username;
+		$_SESSION['password']=$password;
+
+		header('location:Index.php');
+}else{
+
+	echo 'This User Does Not Exist!';
+
+}
+}
+}
+?>

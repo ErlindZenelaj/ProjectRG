2e3f29fc8fe54a728abdd042c245e17a97696059
Erlind <81335694+ErlindZenelaj@users.noreply.github.com>
2022-01-29 18:51:18 +0100

    Search Function Update

--- javascript.js
+++ javascript.js
@@ -5,16 +5,152 @@ function openPage() {
         window.open("../ProjectRG/Filmat/Batman.html", "_self");
     }
 
-    if (x.toLowerCase === "Spiderman".toLowerCase()) {
+    if (x.toLowerCase() === "Spiderman no way home".toLowerCase()) {
         window.open("../ProjectRG/Filmat/SpiderMan1.html", "_self");
     }
 
-    if (x === "Venom") {
+    if (x.toLowerCase() === "Venom".toLowerCase()) {
         window.open("../ProjectRG/Filmat/Venom.html", "_self");
     }
 
+    if (x.toLowerCase() === "Black Widow".toLowerCase()) {
+        window.open("../ProjectRG/Filmat/BlackWidow.html", "_self");
+    }
+
+    if (x.toLowerCase() === "Enternals".toLowerCase()) {
+        window.open("../ProjectRG/Filmat/Enternals.html", "_self");
+    }
+
+    if (x.toLowerCase() === "F9".toLowerCase()) {
+        window.open("../ProjectRG/Filmat/F9.html", "_self");
+    }
+
+    if (x.toLowerCase() === "Free Guy".toLowerCase()) {
+        window.open("../ProjectRG/Filmat/Free-Guy.html", "_self");
+    }
+
+    if (x.toLowerCase() === "Halloween Kills".toLowerCase()) {
+        window.open("../ProjectRG/Filmat/HalloweenKills.html", "_self");
+    }
+
+    if (x.toLowerCase() === "No Time To Die".toLowerCase()) {
+        window.open("../ProjectRG/Filmat/NoTimeToDie.html", "_self");
+    }
+
+    if (x.toLowerCase() === "Shang Chi".toLowerCase()) {
+        window.open("../ProjectRG/Filmat/Shang-Chi.html", "_self");
+    }
+
+    if (x.toLowerCase() === "The Suicide Squad".toLowerCase()) {
+        window.open("../ProjectRG/Filmat/TheSuicideSquad.html", "_self");
+    }
+
+    if (x.toLowerCase() === "SpiderMan Far From Home".toLowerCase()) {
+        window.open("../ProjectRG/Filmat/SpiderManNWH.html", "_self");
+    }
+
+    if (x.toLowerCase() === "SpiderMan Far From Home".toLowerCase()) {
+        window.open("../ProjectRG/Filmat/SpiderManNWH.html", "_self");
+    }
+
+    if (x.toLowerCase() === "La Casa De Papel".toLowerCase()) {
+        window.open("../ProjectRG/Seriale/LaCasaDePapel.html", "_self");
+    }
+
+    if (x.toLowerCase() === "Loki".toLowerCase()) {
+        window.open("../ProjectRG/Seriale/Loki.html", "_self");
+    }
+
+    if (x.toLowerCase() === "Lucifer".toLowerCase()) {
+        window.open("../ProjectRG/Seriale/Lucifer.html", "_self");
+    }
+
+    if (x.toLowerCase() === "Outer Banks".toLowerCase()) {
+        window.open("../ProjectRG/Seriale/OuterBanks.html", "_self");
+    }
+
+    if (x.toLowerCase() === "Queens Gambit".toLowerCase()) {
+        window.open("../ProjectRG/Seriale/QueensGambit.html", "_self");
+    }
+
+    if (x.toLowerCase() === "Rrushe".toLowerCase()) {
+        window.open("../ProjectRG/Seriale/Rrushe.html", "_self");
+    }
+
+    if (x.toLowerCase() === "See".toLowerCase()) {
+        window.open("../ProjectRG/Seriale/See.html", "_self");
+    }
+
+    if (x.toLowerCase() === "Sex Education".toLowerCase()) {
+        window.open("../ProjectRG/Seriale/SexEducation.html", "_self");
+    }
+
+    if (x.toLowerCase() === "Squid Games".toLowerCase()) {
+        window.open("../ProjectRG/Seriale/SquidGames.html", "_self");
+    }
+
+    if (x.toLowerCase() === "The Flash".toLowerCase()) {
+        window.open("../ProjectRG/Seriale/TheFlash.html", "_self");
+    }
+
+    if (x.toLowerCase() === "Vis a Vis".toLowerCase()) {
+        window.open("../ProjectRG/Seriale/Vis-a-Vis.html", "_self");
+    }
+
+    if (x.toLowerCase() === "What If".toLowerCase()) {
+        window.open("../ProjectRG/Seriale/WhatIf.html", "_self");
+    }
+
+    if (x.toLowerCase() === "Attack On Titan".toLowerCase()) {
+        window.open("../ProjectRG/Anime/AttackOnTitan.html", "_self");
+    }
+
+    if (x.toLowerCase() === "Bleach".toLowerCase()) {
+        window.open("../ProjectRG/Anime/Bleach.html", "_self");
+    }
+
+    if (x.toLowerCase() === "Death Note".toLowerCase()) {
+        window.open("../ProjectRG/Anime/DeathNote.html", "_self");
+    }
+
+    if (x.toLowerCase() === "Demon Slayer".toLowerCase()) {
+        window.open("../ProjectRG/Anime/DemonSlayer.html", "_self");
+    }
+
+    if (x.toLowerCase() === "Dragon Ball Super".toLowerCase()) {
+        window.open("../ProjectRG/Anime/DragonBallSuper.html ", "_self");
+    }
+
+    if (x.toLowerCase() === "Dragon Ball Z".toLowerCase()) {
+        window.open("../ProjectRG/Anime/DragonBallZ.html", "_self");
+    }
+
+    if (x.toLowerCase() === "Jujutsu.html".toLowerCase()) {
+        window.open("../ProjectRG/Anime/Jujutsu.html", "_self");
+    }
+
+    if (x.toLowerCase() === "My Hero Academy".toLowerCase()) {
+        window.open("../ProjectRG/Anime/MyHeroAcademy.html", "_self");
+    }
+
+    if (x.toLowerCase() === "Naruto".toLowerCase()) {
+        window.open("../ProjectRG/Anime/Naruto.html", "_self");
+    }
+
+    if (x.toLowerCase() === "Naruto Shippuden".toLowerCase()) {
+        window.open("../ProjectRG/Anime/NarutoShippuden.html", "_self");
+    }
+
+    if (x.toLowerCase() === "One Piece".toLowerCase()) {
+        window.open("../ProjectRG/Anime/OnePiece.html", "_self");
+    }
+
+    if (x.toLowerCase() === "One Punch Man".toLowerCase()) {
+        window.open("../ProjectRG/Anime/OnePunchMan.html", "_self");
+    }
 
     else {
+        window.open("../ProjectRG/Error.html");
         
     }
 }

4a7f96920c1451c9d4ff207298bf691c06d5ceec
Erlind <81335694+ErlindZenelaj@users.noreply.github.com>
2022-01-29 16:55:13 +0100

    JavaScript Search Function

---
+++ javascript.js
@@ -0,0 +1,20 @@
+function openPage() {
+    var x = document.getElementById("search").value;
+
+    if (x.toLowerCase() === "Batman".toLowerCase()) {
+        window.open("../ProjectRG/Filmat/Batman.html", "_self");
+    }
+
+    if (x.toLowerCase === "Spiderman".toLowerCase()) {
+        window.open("../ProjectRG/Filmat/SpiderMan1.html", "_self");
+    }
+
+    if (x === "Venom") {
+        window.open("../ProjectRG/Filmat/Venom.html", "_self");
+    }
+
+
+    else {
+        
+    }
+}

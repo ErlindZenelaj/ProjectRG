820942cbed72ad3d13070b8cd8b30b1fd475744d
Erlind <81335694+ErlindZenelaj@users.noreply.github.com>
2022-02-12 00:11:18 +0100

    .

--- database/databaseConnection.php
+++ database/databaseConnection.php
@@ -4,7 +4,7 @@ class DatabaseConnection{
     private $server="localhost";
     private $username="root";
     private $password="";
-    private $database="test";
+    private $database="test1";
 
    
 
